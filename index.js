9b166875c1f007d3d388cdbc9b60b40dba32aa88
JAVIER GÓMEZ GARCÍA <Javier.Gomez21@alu.uclm.es>
2022-09-27 09:39:50 +0200

    creacion de index.js y paquetes de dependencia

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 'use strict';
 const fs = require('express');
 const express = require('express');
-const uuid = require('uuid/v4');
+//const uuid = require('uuid/v4');
 const app = express();
+
+//HTTP GET POST PUT DELETE
+/*
+
+get "/"
+get "/obtenerPartidas"
+post get (depende de cuanta info le mande al server) "/agregarUsuario/:nick"
+put "/actualizarPartida"
+delete "/eliminarPartida"
+...
+
+*/
+
 app.get('/', (req, res) => {
   res
     .status(200)
@@ -10,9 +23,9 @@ app.get('/', (req, res) => {
     .end();
 });
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+  console.log(`App escuchando en el puerto ${PORT}`);
+  console.log('Press Ctrl+C para salir.');
 });
 // [END gae_flex_quickstart]
