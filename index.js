4fa1e7a675f66aac45cbe50a3dc43d67f2e343fb
JAVIER GÓMEZ GARCÍA <Javier.Gomez21@alu.uclm.es>
2022-10-03 13:01:44 +0200

    llamada de distintos metodos en REST API(index.js)

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
+
 'use strict';
-const fs = require('express');
+const fs = require('fs');
+//const fs = require('express');
 const express = require('express');
 //const uuid = require('uuid/v4');
 const app = express();
-
-//HTTP GET POST PUT DELETE
-/*
-
-get "/"
-get "/obtenerPartidas"
-post get (depende de cuanta info le mande al server) "/agregarUsuario/:nick"
-put "/actualizarPartida"
-delete "/eliminarPartida"
-...
-
-*/
-
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send("Hola")
-    .end();
-});
-// Start the server
 const PORT = process.env.PORT || 3001;
+const modelo = require ("./servidor/modelo.js");
+
+let juego = new modelo.Juego();
+
+
+app.use(express.static(__dirname + "/"));
+
+app.get("/", function(request,response){
+	var contenido=fs.readFileSync(__dirname+"/cliente/index.html");
+	response.setHeader("Content-type","text/html");
+	response.send(contenido);
+});
+
+app.get("/agregarUsuario/:nick",function(request,response){
+  let nick = request.params.nick;
+  let res;
+  res=juego.agregarUsuario(nick);
+  response.send(res);
+});
+
+app.get("/crearPartida/:nick",function(request,response){
+  let nick = request.params.nick;
+  let res = juego.jugadorCreaPartida(nick);
+
+  /*let usr  = juego.usuarios[nick]
+  if(usr){
+    codigo=usr.crearPartida();
+    res = {codigo:codigo};
+  }*/  //Hecho asi al principio por poner la logica aqui que no deberia
+
+  response.send(res);
+});
+
+app.get("/unirseAPartida/:codigo/:nick",function(request,response){
+  let nick = request.params.nick;
+  let codigo = request.params.codigo;
+  let res = juego.jugadorSeUneAPartida(codigo,nick)
+  response.send(res);
+})
+
+app.get("/obtenerPartidas",function(request,response){
+
+  let lista = juego.obtenerPartidas();
+  
+  response.send(lista);
+})
+
+app.get("/obtenerPartidasDisponibles",function(request,response){
+
+  let lista = juego.obtenerPartidasDisponibles();
+  
+  response.send(lista);
+})
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`App escuchando en el puerto ${PORT}`);
   console.log('Press Ctrl+C para salir.');
---
+++ servidor/modelo.js
@@ -0,0 +1,131 @@
+function Juego(){
+    this.partidas={};  
+    this.usuarios={}; //array asociativo (diccionario clave/valor)
+
+    this.agregarUsuario=function(nick){
+        let res={nick:-1};
+        if(!this.usuarios[nick]){
+            this.usuarios[nick]= new Usuario(nick,this); //al crear el usuario le paso el juego
+            res={nick:nick};
+            console.log("Nuevo usuario: "+ nick);
+        }
+        return res;
+    }
+
+    this.eliminarUsuario=function(nick){
+        delete this.usuarios[nick];
+    }
+
+    this.jugadorCreaPartida=function(nick){
+        
+        let res = {codigo:-1}
+        let usr  = this.usuarios[nick]
+        let codigo;
+
+        if(usr){
+            codigo=usr.crearPartida();
+            res = {codigo:codigo};
+        }
+        return res;
+    }
+
+    this.crearPartida=function(jugador){
+        //obtener codigo
+        //crear partida con propietario nick
+        //devolver codigo
+        let codigo=Date.now();
+        this.partidas[codigo]= new Partida(codigo,jugador);
+        console.log("partida creada por "+jugador.nick+ " con codigo "+ codigo);
+        return codigo;
+
+    }
+    this.jugadorSeUneAPartida=function(codigo,nick){
+        let res = {codigo:-1}
+        let usr = this.usuarios[nick]
+        
+
+        if(usr){
+            usr.unirseApartida(codigo);
+            console.log("El usuario " + nick + " se ha unido a la partida " + codigo);
+            res = {codigo:nick};
+        }
+
+        return res;
+
+    }
+    this.unirApartida=function(codigo,jugador){
+        if (this.partidas[codigo]){//si existe o no
+           this.partidas[codigo].agregarJugador(jugador); 
+        }
+        else{
+            console.log("Partida no existe")
+        }
+    }
+    this.obtenerPartidas=function(){
+        let lista=[];
+        for (let key in this.partidas){
+            lista.push({codigo:key,owner:this.partidas[key].owner.nick})
+        }
+        return lista;   
+    }
+    this.obtenerPartidasDisponibles=function(){
+        //devolver solo las partidas que no estan completas
+        //Para mostrarle al usuario solo las disponibles
+        let lista=[];
+        for (let key in this.partidas){
+            if(this.partidas[key].jugadores.length<2){
+                lista.push({codigo:key,owner:this.partidas[key].owner.nick})
+            }
+        }
+        return lista; 
+    }
+}
+
+function Usuario(nick,juego){
+    this.nick=nick;
+    this.juego=juego;
+    this.crearPartida=function(){   //delego en juego
+        return this.juego.crearPartida(this)
+    }
+
+    this.unirseApartida=function(codigo){
+        this.juego.unirApartida(codigo,this);
+    }
+
+}
+
+function Partida(codigo,jugador){
+    this.codigo=codigo;
+    this.owner=jugador;
+    this.jugadores=[]; //array normal o asociativo??
+    //this.maxJugadores=2;
+    this.fase="inicial"; //new Inicial() si decido hacerlo con patron State, en el diagrama ambas opciones disponibles
+    
+    this.agregarJugador= function(jugador){  //modificado para que se agregue el jugador entero no solo el nick
+        let res=this.codigo;
+        if(this.hayHueco){
+            this.jugadores.push(jugador);
+            
+        }
+        else {
+            res= -1;
+            console.log("partida completa");
+        }
+        return res;
+    }
+
+    this.comprobarFase = function(){
+        if(!this.hayHueco()){
+            this.fase= "jugando"
+        }
+    }
+
+    this.hayHueco = function(){
+        return(this.jugadores.length<2)
+    }
+    
+    this.agregarJugador(this.owner);
+
+}
+
+module.exports.Juego = Juego;
